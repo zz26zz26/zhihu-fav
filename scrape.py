1558804ddc897cd2814dcd8f2cecad0fdc4b647c
zz26zz26 <37033499+zz26zz26@users.noreply.github.com>
2018-03-08 22:03:23 +0800

    scrape.py
    修正获取用户名错误
    添加超时重试

--- scrape.py
+++ scrape.py
@@ -1,37 +1,40 @@
-﻿import http.cookiejar
-import urllib.request
-import html.parser
-import sqlite3
-import time
 import os
+import time
+import sqlite3
+import html.parser
+import urllib.request
+
 
 class fav_parser(html.parser.HTMLParser):
-    answer_head = []
-    answer_user = []
-    answer_data = []
-    answer_link = []
-    folder_name = '' # 收藏夹名称
-    next_link = ''   # 只有双引号的字符串可用单引号包住(反过来也行)，不必用r'...'
-    __tmp_link = ''  # 页码区遇到下一页链接的标签前会碰到别的页码
-    __tag_type = 0   # 0-不管 1-页码/div/span/a 2-收藏夹名/h2 3-题目/h2/a 4-答主/div/span/a 5-内容/textarea
+    def __init__(self):        # 放外面所有被实例共用 即新对象也有之前的答案
+        html.parser.HTMLParser.__init__(self)
+        self.folder_name = ''  # 收藏夹名
+        self.answer_head = []  # 问题
+        self.answer_user = []  # 答主
+        self.answer_data = []  # 内容
+        self.answer_link = []  # 链接
+        self.next_link  = ''   # 下一页链接
+        self.__tmp_link = ''   # 页码区遇到下一页链接的标签前会碰到别的页码链接
+        self.__tag_type = 0    # 1-页码/div/span/a; 2-收藏夹名/h2; 3-题目/h2/a; 4-答主/div/span/a; 5-内容
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'textarea':
+        if tag == 'textarea':  # 只有双引号的字符串可用单引号包住(反过来也行), 不必用r'...'
             self.__tag_type = 5
-        elif tag == 'a' and ('data-hovercard') in sum(attrs, ()):     # 专栏/回答有 匿名无; sum转一维大法
+        elif tag == 'span' and 'author-link-line' in sum(attrs, ()):  # 专栏/回答有 匿名/点赞无; sum转一维
             self.__tag_type = 4
-        elif tag == 'h2' and ('class', 'zm-item-title') in attrs:     # 要在下面改[-1]之前append
+        elif tag == 'h2' and ('class', 'zm-item-title') in attrs:     # 要在下面改[-1]之前append 默认匿名
             self.__tag_type = 3;  self.answer_user.append('')
         elif tag == 'h2' and ('id', 'zh-fav-head-title') in attrs:    # 私密收藏夹里面有个<i>图标不影响
             self.__tag_type = 2
-        elif tag == 'div' and ('class', 'zm-invite-pager') in attrs:  # 恰好div里面没别的div，直接用type判
-            self.__tag_type = 1  # 只有一页时进不来此if，next_link不变
+        elif tag == 'div' and ('class', 'zm-invite-pager') in attrs:  # 恰好div里面没别的div, 直接用type判
+            self.__tag_type = 1  # 只有一页时进不来此if, next_link不变
 
         if self.__tag_type == 1 and tag == 'a':
             self.__tmp_link = dict(attrs)['href']
         elif self.__tag_type == 4 and tag == 'a':
-            url = dict(attrs)['href']                      # 原为/people/xxx
-            self.answer_user[-1] = url[url.rfind('/')+1:]  # 改为xxx
+            url = dict(attrs)['href']                      # 原为/people/xxxx
+            self.answer_user[-1] = url[url.rfind('/')+1:]  # 改为xxxx
+            self.__tag_type = 0                            # 如果在end才置0会让提取下页链接时提前退出
         elif tag == 'div' and 'data-entry-url' in sum(attrs, ()):
             url = dict(attrs)['data-entry-url']            # 只有图片的回答没有[显示全部] 专栏没有[编辑于..]
             if url[0] == '/':                              # 专栏自带完整链接
@@ -53,17 +56,18 @@ class fav_parser(html.parser.HTMLParser):
             self.answer_data.append(data)
 
     def handle_endtag(self, tag):
-        if self.__tag_type > 0 and tag in ('div', 'h2', 'textarea'):
+        if self.__tag_type > 0 and tag in ('h2', 'div', 'textarea'):
             self.__tag_type = 0  # 优先级: is(内存地址) > in > not > and > or
 
-# When scraping 'answer_user': (进入答案/文章页才能得到头像文件名，比用用户名除重难)
-# <img class="Avatar Avatar--large UserAvatar-inner" src="xxx_xl.jpg" ... 用户页(自带两条动态) X
-# <img class="Avatar AuthorInfo-avatar" src="xxx_xs.jpg" ... 动态|问题页(更多回答和关于作者栏均由js载入) 快！
-# <img class="Avatar-hemingway PostIndex-authorAvatar Avatar--xs" src="xxx_xs.jpg" ... 专栏文章 X
+
+# For scraping 'answer_user': (进答案/文章页才有头像文件名, 用文件名除重比用户名复杂)
+# <img class="Avatar Avatar--large UserAvatar-inner" src="xxx_xl.jpg" 用户页(自带两条动态) X
+# <img class="Avatar AuthorInfo-avatar" src="xxx_xs.jpg" 动态|问题页(更多回答和关于作者栏由js载入) 快！
+# <img class="Avatar-hemingway PostIndex-authorAvatar Avatar--xs" src="xxx_xs.jpg" 专栏文章
 class img_parser(html.parser.HTMLParser):
     def __init__(self):
-        self.link = ''  # 放外面是类变量了，所有实例共用，外部也可用类名访问
         html.parser.HTMLParser.__init__(self)
+        self.link = ''
     
     def feed(self, data):
         self.link = ''  # 防止返回上次得到的地址
@@ -74,84 +78,131 @@ class img_parser(html.parser.HTMLParser):
             if ('class', 'Avatar AuthorInfo-avatar') in attrs or \
                ('class', 'Avatar-hemingway PostIndex-authorAvatar Avatar--xs') in attrs:
                 url = dict(attrs)['src']
-                url = url[0:url.rfind('_')] + url[url.rfind('.'):]  # 没扩展名是png
+                url = url[0:url.rfind('_')] + url[url.rfind('.'):]  # 没扩展名得png, 太大
                 self.link = url
 
 
-########## Prepare for Scraping ##########
-fav = fav_parser()
-fav.next_link = '?page=1'
-entry_url = 'https://www.zhihu.com/collection/186197750'
-cookie = ''
-header = {'Cookie': cookie}
+##########################
+#####   Interfaces   #####
+##########################
+def get_data(entry_url, header):
+    fav = fav_parser()
+    fav.next_link = '?page=1'
 
-
-########## Collection Folder Scraping Begins ##########
-while len(fav.next_link) > 0:
-    print('Requesting', fav.next_link[1:], end=' ')
-    t0 = time.clock()
-    request = urllib.request.Request(entry_url + fav.next_link, headers=header)
-    response = urllib.request.urlopen(request)
-    page_html = response.read().decode()
-    print('returns status code %d (%.3fs)' % (response.status, time.clock() - t0), end=' ')
-
-    fav.next_link = ''
-    fav.feed(page_html)
-    if len(fav.answer_head) == len(fav.answer_user) and \
-       len(fav.answer_user) == len(fav.answer_link) and \
-       len(fav.answer_link) == len(fav.answer_data):
-        time.sleep(1.5)
-        print('')
-    else:
-        print('-', len(fav.answer_head), len(fav.answer_user), len(fav.answer_link), len(fav.answer_data))
-
-insert_rows = []
-for i in range(len(fav.answer_data)):
-    insert_rows.append((fav.folder_name, fav.answer_head[i], fav.answer_user[i],
-                        fav.answer_link[i], fav.answer_data[i]))
-print('Find', len(fav.answer_data), 'collection items')
-
-conn = sqlite3.connect('fav.db')
-# conn.execute('''CREATE TABLE fav (folder  TEXT,
-#                                   title   TEXT,
-#                                   author  TEXT,
-#                                   link    TEXT,
-#                                   content TEXT);''')  # 表/列名有空格才要放在``里
-# conn.execute('DELETE FROM fav WHERE folder="测试";')
-conn.executemany('INSERT INTO fav VALUES (?,?,?,?,?);', insert_rows)
-conn.commit()
-conn.close()
-# exit()
-
-########## Image Scraping Begins ##########
-img = img_parser()
-failed = []
-existed = [f[0:-4] for f in os.listdir('./avatar')]  # 去掉.jpg
-new_user = set(fav.answer_user) - set(existed)  # 集合除重，再除已下载的头像文件
-print('Find', len(new_user), 'new avatars')
-
-for user in new_user:
-    t0 = time.clock()
-    link = fav.answer_link[fav.answer_user.index(user)]  # 答案页比用户页载入快
-    print("Requesting avatar of", user, end=' ')
-    request = urllib.request.Request(link, headers=header)  # 有的用户/问题登录才能看，要cookie
-    response = urllib.request.urlopen(request)
-    page_html = response.read().decode()
-
-    img.feed(page_html)
-    if len(img.link) > 0:
-        request = urllib.request.Request(img.link, headers={'Referer': link})  # 图片防盗链，头部与爬网页不同
+    while len(fav.next_link) > 0:
+        print('Requesting', fav.next_link[1:], end=' ')
+        t0 = time.clock()
+        request = urllib.request.Request(entry_url + fav.next_link, headers=header)
         response = urllib.request.urlopen(request)
+        print('(%.3fs)' % (time.clock() - t0), end=' ')
         if response.status != 200:
-            print('returns %d' % (response.status), end=' ')
+            print('(status code %d)' % (response.status, ), end=' ')
+            continue  # fav.next_link还没改 正好重来; 而且网页有问题时没下页链接
+
+        page_html = response.read().decode()  # 有问题就read会抛异常
+        fav.next_link = ''
+        fav.feed(page_html)
+        if len(fav.answer_head) == len(fav.answer_user) and \
+           len(fav.answer_user) == len(fav.answer_link) and \
+           len(fav.answer_link) == len(fav.answer_data):
+            print(''); time.sleep(2)  # 1s太短可能被搞
+        else:
+            print('-', len(fav.answer_head), len(fav.answer_user), len(fav.answer_link), len(fav.answer_data))
+    
+    return fav
+
+
+def update_database(fav):
+    fav_rows = []  # 一次更新的都属于同一个收藏夹
+    for i in range(len(fav.answer_data)):
+        fav_rows.append((fav.folder_name, fav.answer_head[i], fav.answer_user[i],
+                            fav.answer_link[i], fav.answer_data[i]))  # 每行是一个(tuple)
+    print('Found', len(fav.answer_data), 'collection items')
+
+    conn = sqlite3.connect('fav.db')
+    columns = conn.execute('PRAGMA table_info(fav);').fetchall()  # 表中每列属性，无此表返回空
+    if len(columns) != 5:
+        conn.execute('''ALTER TABLE `fav` RENAME TO `fav_old`;''')  # 表/列名有.空格/关键字放在反引号``里
+        conn.execute('''CREATE TABLE fav (folder  TEXT,
+                                          title   TEXT,
+                                          author  TEXT,
+                                          link    TEXT,
+                                          content TEXT,
+                                          PRIMARY KEY(folder, link));''')
+    # conn.execute('CREATE UNIQUE INDEX ifav on fav (folder, link);') 建表时没弄唯一约束才用/索引的列应少插改
+
+    old = conn.execute('SELECT link, content FROM fav WHERE folder="%s";' % fav.folder_name).fetchall()
+    old_lnk = [r[0] for r in old]
+    new_lnk = [r[3] for r in fav_rows]
+    insert = [r for r in fav_rows if r[3] not in old_lnk]  # 递推式/推导式 [3:5]的区间是[3,5)
+    update = [r for r in fav_rows if r[3] in old_lnk and r[3:5] not in old]  # 内容没变的去掉
+    delete = [r for r in old if r[0] not in new_lnk]
+    print('Including %d new, %d edited and found %d deleted items.' % (len(insert), len(update), len(delete)))
+
+    # REPLACE不能用WHERE, 唯一(组合)索引一致就先删再插(显然没变的也删) (primary key/unique/unique index都看)
+    conn.executemany('INSERT OR REPLACE INTO fav VALUES (?,?,?,?,?);', fav_rows)
+    conn.commit()
+    conn.close()
+
+
+def get_avatar(fav, header):
+    img = img_parser()
+    failed = []
+    existed = [f[0:-4] for f in os.listdir('./avatar')]  # 去掉'.jpg'
+    new_user = set(fav.answer_user) - set(existed)  # 集合除重，再除已下载的头像文件
+    print('Found', len(new_user), 'new avatars')
+
+    def check_status(response):
+        if response.status != 200:
+            print('(code %d)' % (response.status), end=' ')
             failed.append(user)
+            return 1  # fails
+        else:
+            return 0
+
+    for user in new_user:
+        print("Requesting avatar of", user, end=' ')
+        t0 = time.clock()
+        link = fav.answer_link[fav.answer_user.index(user)]  # 答案页比用户页载入快
+        request = urllib.request.Request(link, headers=header)  # 有的用户/问题登录才能看 要cookie
+        response = urllib.request.urlopen(request)
+        if check_status(response): continue
+        
+        img.feed(response.read().decode())
+        if len(img.link) == 0: failed.append(user); continue
+        
+        request = urllib.request.Request(img.link, headers={'Referer': link})
+        response = urllib.request.urlopen(request) # 图片防盗链, 头部与爬网页不同
         print('(%dk)' % (int(response.length / 1024)), end=' ')
+        print('(%.3fs)' % (time.clock() - t0))
+        if check_status(response): continue
+        
         pic = open('avatar/' + user + '.jpg', 'wb')  # 文件夹得先建好
         pic.write(response.read())  # read完response.length就变0了
         pic.close()
-    else:
-        print('but failed', end=' ')
-        failed.append(user)
-    print('(%.3fs)' % (time.clock() - t0))
 
-print(len(failed), 'avatar failed')
+    print(len(failed), 'avatar failed')
+
+
+##########################
+#####   Run Script   #####
+##########################
+if __name__ == '__main__':  # 脚本模式运行此文件时进入; F12后复制主html的cookie即可
+    header = {'Cookie': ''}
+
+    all_fav = 'https://www.zhihu.com/collections/mine'  # 个人主页的收藏只自带4个 其余动态加载
+    request = urllib.request.Request(all_fav, headers=header)
+    response = urllib.request.urlopen(request)
+    page_html = response.read().decode()
+
+    fav_entry = []
+    next_entry = page_html.find('/collection/')
+    while next_entry >= 0:
+        fav_entry.append('https://www.zhihu.com' + page_html[next_entry:page_html.find('"', next_entry)])
+        next_entry = page_html.find('/collection/', next_entry + 1)
+
+    for entry in fav_entry[:]:
+        print('\n' + entry)
+        fav = get_data(entry, header)
+        update_database(fav)
+        get_avatar(fav, header)
