ec9b6d26ffd4e05ec275840cfe779855356e4d98
zz26zz26 <zz26zz26@yeah.net>
2018-07-07 22:52:25 +0800

    Fix a bug when the folder contains Zhihu Pin (scrape.py)
    修正收藏夹里有知乎想法时标题内容不匹配的问题(想法没标题)

--- scrape.py
+++ scrape.py
@@ -21,8 +21,9 @@ class fav_parser(html.parser.HTMLParser):
         self.__tmp_link = ''   # 页码区遇到下一页链接的标签前会碰到别的页码链接
         self.__tag_type = 0    # 1-页码/div/span/a; 2-收藏夹名/h2; 3-题目/h2/a; 4-答主/div/span/a; 5-内容
 
-    def handle_timestamp(self, text):  # 格式: 发布/编辑于 2018-01-01 / 昨天 00:00 / 00:00 (即今天)
-        text = text[text.find('于')+1:].strip()
+    def handle_timestamp(self, text):  # 格式: 发布/编辑于 2018-01-01 00:00 / 昨天 00:00 / 00:00 (即今天)
+        text = text[text.find('于') + 1:].strip()
+        text = text[:text.find(' ') if ' ' in text else len(text)]
         if '昨天' in text:
             text = time.strftime("%Y-%m-%d", time.localtime(time.time() - 86400))
         elif ':' in text:
@@ -43,40 +44,44 @@ class fav_parser(html.parser.HTMLParser):
         elif tag == 'div' and ('class', 'zm-invite-pager') in attrs:  # 恰好div里面没别的div, 直接用type判
             self.__tag_type = 1  # 只有一页时进不来此if, next_link不变
 
-        if self.__tag_type == 1 and tag == 'a':
+        if tag == 'a' and (self.__tag_type in (1, 4)):  # 下一页(1)和答主主页(4)都是链接
             self.__tmp_link = dict(attrs)['href']
-        elif self.__tag_type == 4 and tag == 'a':
-            url = dict(attrs)['href']                      # 原为/people/xxxx
-            self.answer_user[-1] = url[url.rfind('/')+1:]  # 改为xxxx
-        elif self.__tag_type == 3 and tag != 'a':
-            self.answer_user.append('')                    # 遇标题就加一空白项 (<h2>里文字外有个<a>不要再加)
-            self.answer_name.append('')                    # 等读到别的标签或读完html再填数
+        elif tag == 'div' and ('class', 'zm-item') in attrs:
+            self.answer_head.append('')                 # 遇到新的先加个空白项 (想法没标题)
+            self.answer_user.append('')
+            self.answer_name.append('')                 # 等读到相应的标签或读完html再填内容
+            self.answer_text.append('')
+            self.answer_link.append('')
             self.answer_time.append('')
         elif tag == 'div' and 'data-entry-url' in sum(attrs, ()):
-            url = dict(attrs)['data-entry-url']            # 纯图片回答没有[显示全部] 专栏没有[编辑于]
-            if url[0] == '/':                              # 专栏自带完整链接 只需去掉https://
-                url = 'www.zhihu.com' + url
+            url = dict(attrs)['data-entry-url']         # 纯图片回答没有[显示全部] 专栏没有[编辑于]
+            if url[0] == '/':
+                url = 'www.zhihu.com' + url             # 回答和想法的链接是相对路径
             elif '://' in url:
-                url = url[url.find('://') + 3:]
-            self.answer_link.append(url)
+                url = url[url.find('://') + 3:]         # 专栏自带完整链接 只需去掉https://
+            self.answer_link[-1] = url
 
     def handle_data(self, data):
         next_text = '下一页'
-        if self.__tag_type == 0:                              # 省得下面判断那么多
+        data = data.strip()
+        if self.__tag_type == 0 or len(data) == 0:        # 省得下面判断那么多
             pass
-        elif self.__tag_type == 1 and data != next_text:      # 最后一页的[下一页]没链接
+        elif self.__tag_type == 1 and data != next_text:  # 最后一页的[下一页]没链接
             self.__tmp_link = ''
-        elif self.__tag_type == 1 and data == next_text:      # 若留着之前页码的链接会持续访问倒数第二页
+        elif self.__tag_type == 1 and data == next_text:  # 若留着之前页码的链接会持续访问倒数第二页
             self.next_link = self.__tmp_link
-        elif self.__tag_type == 2:                            # 收藏夹名前后有\n
-            self.folder_name = data.strip()
-        elif self.__tag_type == 3 and len(data) > 0:          # 标题在<h2>中的<a>里 跳过无内容标签
-            self.answer_head.append(data)
-        elif self.__tag_type == 4 and len(data.strip()) > 0:  # 若endtag才置0会让提取下页链接时提前退出
-            self.answer_name[-1] = data.strip();  self.__tag_type = 0
+        elif self.__tag_type == 2:                        # 收藏夹名前后有\n
+            self.folder_name = data
+        elif self.__tag_type == 3:                        # 标题在<h2>中的<a>里 跳过无内容标签
+            self.answer_head[-1] = data
+        elif self.__tag_type == 4:
+            url = self.__tmp_link
+            self.answer_name[-1] = data                   # 答主主页为/people/xxxx, 改为xxxx
+            self.answer_user[-1] = url[url.rfind('/') + 1:]
+            self.__tag_type = 0                           # 若让endtag对span也置0会让提取下页链接时提前退出
         elif self.__tag_type == 5:
-            self.answer_text.append(data)
-        elif self.__tag_type == 6 and len(data) > 0:
+            self.answer_text[-1] = data
+        elif self.__tag_type == 6:
             self.answer_time[-1] = self.handle_timestamp(data)
 
     def handle_endtag(self, tag):
@@ -86,8 +91,9 @@ class fav_parser(html.parser.HTMLParser):
 
 # For scraping 'answer_user': (进答案/文章页才有头像文件名, 用文件名除重比用户名复杂)
 # <img class="Avatar Avatar--large UserAvatar-inner" src="xxx_xl.jpg" 用户页(自带两条动态) X
-# <img class="Avatar AuthorInfo-avatar" src="xxx_xs.jpg" 动态|问题页(更多回答和关于作者栏由js载入) 快！
+# <img class="Avatar AuthorInfo-avatar" src="xxx_xs.jpg" 动态|问题页(更多回答和关于作者栏由js载入)
 # <img class="Avatar Avatar--round AuthorInfo-avatar" src="xxx_xs.jpg" 专栏文章
+# <img class="Avatar-hemingway AuthorIndex-avatar Avatar--xs" src="xxx_xs.jpg" 想法
 # 之前的专栏是Avatar-hemingway PostIndex-authorAvatar Avatar--xs
 class img_parser(html.parser.HTMLParser):
     def __init__(self):
@@ -101,7 +107,8 @@ class img_parser(html.parser.HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == 'img':
             if ('class', 'Avatar AuthorInfo-avatar') in attrs or \
-               ('class', 'Avatar Avatar--round AuthorInfo-avatar') in attrs:
+               ('class', 'Avatar Avatar--round AuthorInfo-avatar') in attrs or \
+               ('class', 'Avatar-hemingway AuthorIndex-avatar Avatar--xs') in attrs:
                 url = dict(attrs)['src']
                 url = url[:url.rfind('_')] + url[url.rfind('.'):]
                 self.link = url
@@ -228,7 +235,7 @@ def export_database():
 
 
 def get_data(entry_url, header, ignore_old = False):
-    ''' 获取收藏夹内的答案数据, 可选只要新增数据 (即忽略收藏后又被修改的答案) '''
+    ''' 获取收藏夹的内容, 可选只要新增项 (即忽略收藏后又被修改的答案) '''
     fav = fav_parser()
     fav.next_link = '?page=1'
 
@@ -247,28 +254,23 @@ def get_data(entry_url, header, ignore_old = False):
         t0 = time.clock()
         fav.next_link = ''
         fav.feed(page_html)
-        print('(feed:%.3fs)' % (time.clock() - t0), end=' ')
-        if len(fav.answer_head) == len(fav.answer_text) and \
-           len(fav.answer_user) == len(fav.answer_text) and \
-           len(fav.answer_link) == len(fav.answer_text) and \
-           len(fav.answer_name) == len(fav.answer_text) and \
-           len(fav.answer_time) == len(fav.answer_text):
-            print('')
-        else:
-            print('-', len(fav.answer_head), len(fav.answer_user), len(fav.answer_link),
-                       len(fav.answer_text), len(fav.answer_time), len(fav.answer_name))
+        print('(feed:%.3fs)' % (time.clock() - t0))
         
         if ignore_old and check_database(fav):
             break
         else:
             time.sleep(2)  # 1s太短可能被搞
 
-    # gif在回答页才直接给出 专栏的日期要进文章页才有
+    return fav
+
+def get_data_detail(fav, header):
+    ''' 获取收藏项的详细信息 (回答页才给gif地址 专栏和想法点进去才有日期) '''
     pattern = re.compile(r'<img[^>]*\ssrc="([^"]*)"[^>]*>')
+
     for i in range(len(fav.answer_link)):
         link = 'https://' + fav.answer_link[i]
         fav_imgs = [m for m in pattern.finditer(fav.answer_text[i])]
-        if 'zhuanlan' not in link and len(fav_imgs) < 1: continue
+        if 'zhuanlan' not in link and 'pin' not in link and len(fav_imgs) < 1: continue
         
         print('Requesting', link, end=' ')
         t0 = time.clock()
@@ -289,18 +291,22 @@ def get_data(entry_url, header, ignore_old = False):
                 pos = fav_img.end(1) + 1  # 在原html中src的引号后插入 保留原src以载入静止图
                 add = ' data-actualsrc="' + page_srcs[idx] + '"'  # 用新属性装gif地址
                 fav.answer_text[i] = fav.answer_text[i][:pos] + add + fav.answer_text[i][pos:]
+            print(' - found %d GIF(s)' % len(page_gifs), end=' ')
 
-        # 文章页的编辑日期提取
+        # 文章/想法页的编辑日期提取
         if 'zhuanlan' in link:  # 找div里的span内的文本(标签中间即>之后<之前)
             time_pos = page_html.find('<div class="ContentItem-time"')
             time_pos = page_html.find('<span ', time_pos)
             time_pos = page_html.find('>', time_pos) + 1
             fav.answer_time[i] = fav.handle_timestamp(page_html[time_pos : page_html.find('<', time_pos)])
+        elif 'pin' in link:
+            time_pos = page_html.find('<div class="HoverTitle"')
+            time_pos = page_html.find('data-hover-title=', time_pos)
+            time_pos = page_html.find('"', time_pos) + 1
+            fav.answer_time[i] = fav.handle_timestamp(page_html[time_pos : page_html.find('"', time_pos)])
 
-        time.sleep(1)
+        time.sleep(1.5)
         print('')
-    
-    return fav
 
 
 def get_avatar(fav, header):
@@ -310,7 +316,7 @@ def get_avatar(fav, header):
 
     img = img_parser()
     failed = []
-    existed = [f[0:-4] for f in os.listdir('./avatar')]  # 去掉'.jpg'
+    existed = [f[0:-4] for f in os.listdir('./avatar')]  # 文件名去掉'.jpg'
     new_user = set(fav.answer_user) - set(existed)  # 集合除重，再除已下载的头像文件
     if len(new_user) > 0:
         print('Find', len(new_user), 'new avatars')
@@ -367,6 +373,7 @@ if __name__ == '__main__':  # 脚本模式运行此文件时进入
     for i in range(0, len(fav_entry)):
         print('\n%s (%s)' % (fav_entry[i], fav_title[i]))
         fav = get_data(fav_entry[i], header, ignore_old=True)
+        get_data_detail(fav, header)
         update_database(fav)
         get_avatar(fav, header)
     
